d6f6c74facf4e6f5f5e6d80d7ae53042f36c4292
huangkc <huangkaichun@maiditech.com>
2017-08-25 17:11:33 +0800

    AlertDialog and ProgressDialog

--- app/src/main/java/com/maidi/uiwidgettest/MainActivity.java
+++ app/src/main/java/com/maidi/uiwidgettest/MainActivity.java
@@ -1,10 +1,14 @@
 package com.maidi.uiwidgettest;
 
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,9 +24,39 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 switch (view.getId()){
                     case R.id.button:
-                        String inputText = editText.getText().toString();
+                       /* String inputText = editText.getText().toString();
                         Toast.makeText(MainActivity.this,inputText,
-                                Toast.LENGTH_SHORT).show();
+                                Toast.LENGTH_SHORT).show();*/
+
+                        /*****AlertDialog begind *****/
+                        /*
+                        AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
+                        dialog.setTitle("This is Dialog");
+                        dialog.setMessage("Something import.");
+                        dialog.setCancelable(false);
+                        dialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface anInterface, int which) {
+
+                            }
+                        });
+                        dialog.setNegativeButton("Cansel", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface anInterface, int i) {
+
+                            }
+                        });
+                        dialog.show();*/
+                        /*****AlertDialog end *****/
+
+                        /*****ProgressDialog begin *****/
+                        ProgressDialog progressDialog = new ProgressDialog(MainActivity.this);
+                        progressDialog.setTitle("This is ProgressDialog");
+                        progressDialog.setMessage("loading...");
+                        progressDialog.setCancelable(true);
+                        progressDialog.show();
+                        /*****ProgressDialog end *****/
+
                         break;
                     default:
                         break;
